9f74c9a337fc95c76eadfb46dc3b18da9ba6dd70
Alvin Uchenna <43749581+Veri5ied@users.noreply.github.com>
2020-10-29 20:18:19 +0100

    backend setup

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
+
+mongoose
+  .connect(
+    "mongodb+srv://alvin:mX112qDtkynye5OZ@cluster0.iugyx.mongodb.net/test?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    console.log("MongoDB Atlas connected succesfully");
+  })
+  .catch((error) => {
+    console.log("Error connecting to mongodb atlas");
+    console.log(error);
+  });
+
+app.use(bodyParser.json());
+
+module.exports = app;
